5263cac2805a611caaeb4458e3f515c7f2555b2d
cmhughes <christopher.michael.hughes@gmail.com>
2018-01-26 20:45:52 +0000

    further test cases

--- test-cases/hello-world.sh
+++ test-cases/hello-world.sh
@@ -3,8 +3,13 @@ echo "hello world"
 echo "again again"
 #perl myfile.pl
 cp environments/environments-nested-fourth.tex ../
+cp environments/env-all-on.yaml ../localSettings.yaml
+cp environments/env-all-on.yaml ../env-all-on.yaml
 cd ../
 perl testing.pl environments-nested-fourth
 perl testing.pl environments-nested-fourth.tex
 perl testing.pl -w environments-nested-fourth.tex
+perl testing.pl -l -m environments-nested-fourth
+perl testing.pl -l=env-all-on -m environments-nested-fourth
+cat environments-nested-fourth.tex|perl testing.pl
 exit
