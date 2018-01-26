1a6b4c95852e6387c21a1e40335ce9aa64d50bf4
cmhughes <christopher.michael.hughes@gmail.com>
2018-01-26 20:51:41 +0000

    renamed test cases

---
+++ test-cases/travis-ci/test-travis-ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "hello world"
+echo "again again"
+#perl myfile.pl
+cp ../environments/environments-nested-fourth.tex ../../
+cp ../environments/env-all-on.yaml ../../localSettings.yaml
+cp ../environments/env-all-on.yaml ../../env-all-on.yaml
+cd ../../
+echo "----------1. basic test--------------\n"
+perl testing.pl environments-nested-fourth.tex
+echo "----------2. no file extension--------------\n"
+perl testing.pl environments-nested-fourth
+echo "----------3. overwrite--------------\n"
+perl testing.pl -w environments-nested-fourth.tex
+echo "----------4. localSettings and modify line breaks--------------\n"
+perl testing.pl -l -m environments-nested-fourth
+echo "----------5. localSettings renamed and modify line breaks--------------\n"
+perl testing.pl -l=env-all-on -m environments-nested-fourth
+echo "----------6. STDIN/cat mode--------------\n"
+cat environments-nested-fourth.tex|perl testing.pl
+exit
