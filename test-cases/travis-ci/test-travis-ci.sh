1521ca92c7d4286d550e2f04e6d3762bee6bcab6
cmhughes <christopher.michael.hughes@gmail.com>
2018-02-10 14:29:28 +0000

    attempting travis deploy: https://docs.travis-ci.com/user/deployment/pages/

--- test-cases/travis-ci/test-travis-ci.sh
+++ test-cases/travis-ci/test-travis-ci.sh
@@ -17,13 +17,13 @@ echo "again again"
 #perl testing.pl -l=env-all-on -m environments-nested-fourth
 #echo "----------6. STDIN/cat mode--------------\n"
 #cat environments-nested-fourth.tex|perl testing.pl
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git checkout master
-cp testing.pl cmhtestfile.pl
-git add cmhtestfile.pl
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-git status
-git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-git remote -v
-git push --set-upstream origin
+#git config --global user.email "travis@travis-ci.org"
+#git config --global user.name "Travis CI"
+#git checkout master
+#cp testing.pl cmhtestfile.pl
+#git add cmhtestfile.pl
+#git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#git status
+#git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+#git remote -v
+#git push --set-upstream origin
